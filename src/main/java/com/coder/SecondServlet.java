52382d620387f2bda4c5f5566abbdc12d80ed207
mr-vikash <mrvikash700@gmail.com>
2022-02-06 23:01:45 +0530

    first Commit

---
+++ src/main/java/com/coder/SecondServlet.java
@@ -0,0 +1,44 @@
+package com.coder;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+		
+		PrintWriter  out = response.getWriter(); 
+		
+		String user_name = request.getParameter("name");
+		
+		out.println("<h2> Second Servlet...</h2>");
+		
+		out.println("<br>");
+		
+		out.println("<h2> Hello, "+user_name+" Welcome  back to my Website.. </h2>");
+	}
+
+}
